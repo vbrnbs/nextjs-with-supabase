6a64997f2b79619545d2e0e89c7d5f7927234ae7
vbrnbs <varszegibarnabas@gmail.com>
2024-06-14 10:07:18 +1200

    fresh start for launch

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "ShipStream",
+  title: "ShipStreamer",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
