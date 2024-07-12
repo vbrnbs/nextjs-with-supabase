32e4a5239154881ff7435d14ae2ee1187706051d
Barnabás Várszegi <60694401+vbrnbs@users.noreply.github.com>
2024-07-12 04:43:04 +0000

    build

--- hooks/useScreenSize.ts
+++ hooks/useScreenSize.ts
@@ -3,15 +3,13 @@
 import { useState, useEffect } from "react";
 
 const useScreenSize = () => {
-//   const [isMobile, setIsMobile] = useState<boolean>;(
-//     typeof window !== "undefined" ? window.innerWidth < 768 : false,
-//   );
-
-//   useEffect(() => {
-//     window.innerWidth < 768 ? setIsMobile(true) : setIsMobile(false);
-//   }, []);
-
-//   return isMobile;
-// };
+  //   const [isMobile, setIsMobile] = useState<boolean>;(
+  //     typeof window !== "undefined" ? window.innerWidth < 768 : false,
+  //   );
+  //   useEffect(() => {
+  //     window.innerWidth < 768 ? setIsMobile(true) : setIsMobile(false);
+  //   }, []);
+  //   return isMobile;
+};
 
 export default useScreenSize;
